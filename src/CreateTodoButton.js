a89dfa3f3b0de710fd8dbac54798d9c88387b645
cristhyan <cristhyan.camilo@gmailcom>
2023-02-15 09:51:49 -0500

    empezando a usar el estado en react

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './CreateTodoButton.css';
 
 function CreateTodoButton(props) {
-  const onClickButton = (msg) => {
-    alert(msg);
+  const onClickButton = () => {
+    alert('Aqui deberia ir un modal');
   };
 
   return (
     <button 
     className="CreateTodoButton"
-    onClick={() => onClickButton('Aqui deberia ir un modal')}
+    onClick={onClickButton}
     >
       
       Añadir tarea
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,17 +2,22 @@ import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+const [searchValue, setSearchValue] = React.useState('');
+
 const onSearchValueChange = (event) => {
   console.log(event);
+  setSearchValue(event.target.value);
 }
 
-  return (
+  return [
     <input 
     className="TodoSearch" 
     placeholder="Agrega tus tareas" 
+    value={searchValue}
     onChange={onSearchValueChange}
-    />
-  );
+    />,
+    <p>{searchValue}</p>
+  ];
 }
 
 export { TodoSearch };
